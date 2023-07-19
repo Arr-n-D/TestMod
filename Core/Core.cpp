7418127da914963580a2038a28aed0c3c52112a0
Renaud <renaudgdumont@gmail.com>
2023-07-18 23:39:05 -0400

    change how we load the player

--- Core/Core.cpp
+++ Core/Core.cpp
@@ -6,8 +6,9 @@
 #include <Unreal/AActor.hpp>
 #include <Unreal/Hooks.hpp>
 #include <Unreal/World.hpp>
-#include "Core.hpp"
 #include <Unreal/UObjectArray.hpp>
+#include <Unreal/UFunction.hpp>
+#include "Core.hpp"
 
 using namespace RC;
 using namespace RC::Unreal;
@@ -27,31 +28,31 @@ namespace ArrND::Core
 
     void Core::OnUpdate()
     {
-        this->FetchPlayer();
+        //this->FetchPlayer();
 
     }
 
     void Core::FetchPlayer()
     {
-        if (!this->IsPlayerReady()) {
-            //print we're running'
-            Output::send<LogLevel::Verbose>(STR("We're running\n"));
-            if (Core::tempPlayer != nullptr)
-            {
-                if (!Core::tempPlayer->IsUnreachable()) {
-                    auto pos = Core::tempPlayer->K2_GetActorLocation();
-                    std::string posx = std::to_string(pos.X());
+        //if (!this->IsPlayerReady()) {
+        //    //print we're running'
+        //    Output::send<LogLevel::Verbose>(STR("We're running\n"));
+        //    if (Core::tempPlayer != nullptr)
+        //    {
+        //        if (!Core::tempPlayer->IsUnreachable()) {
+        //            auto pos = Core::tempPlayer->K2_GetActorLocation();
+        //            std::string posx = std::to_string(pos.X());
 
-                    if (!posx.starts_with("0") && !posx.starts_with("-0.0000"))
-                    {
-                        //print we're in game'
-                        Output::send<LogLevel::Verbose>(STR("We're past the main menus and in-game\n"));
-                        this->SetPlayerObject(Core::tempPlayer);
-                    }
-                }
+        //            if (!posx.starts_with("0") && !posx.starts_with("-0.0000"))
+        //            {
+        //                //print we're in game'
+        //                Output::send<LogLevel::Verbose>(STR("We're past the main menus and in-game\n"));
+        //                this->SetPlayerObject(Core::tempPlayer);
+        //            }
+        //        }
 
-            }
-        }
+        //    }
+        //}
        
     }
 
@@ -69,32 +70,29 @@ namespace ArrND::Core
 		this->Player = Player;
         this->SetPlayerReadyState(true);
 
-        *Core::isPlayerSet = true;
-        Core::tempPlayer = nullptr;
-        foundPlayerHowManyTimes = 0;
+        //*Core::isPlayerSet = true;
+        //Core::tempPlayer = nullptr;
+        //foundPlayerHowManyTimes = 0;
 	}
+
+    //void Core::HandleLoadingScren(UObject* (*)(const FStaticConstructObjectParameters& Params, UObject* ConstructedObject) {
+
+    //}
 
     void Core::OnUnrealInitialized()
     {
-        Unreal::Hook::RegisterBeginPlayPostCallback(Core::OnActorBeginPlay);
-    }
+        //Unreal::Hook::RegisterStaticConstructObjectPostCallback(hook);
 
-    void Core::OnActorBeginPlay(Unreal::AActor *Context)
-    {
-        if (Context->GetName().starts_with(STR("BP_Biped_Player_C")))
-        {
-            
-            foundPlayerHowManyTimes++; 
+        auto posthook = [](Unreal::UnrealScriptFunctionCallableContext& Context, void* CustomData) {
+            auto Player = UObjectGlobals::FindFirstOf(STR("Biped_Player"));
 
-            if (foundPlayerHowManyTimes == 2) {
-                Output::send<LogLevel::Verbose>(STR("PlayerController found\n"));
-                Core::tempPlayer = Context;
+            //if not null ptr 
+            if (Player != nullptr && !Player->IsUnreachable()) {
+                Output::send<LogLevel::Verbose>(STR("Player address: {}\n"), (uintptr_t)Player);
+
             }
+        };
 
-            if (*Core::isPlayerSet != false) {
-                *Core::isPlayerSet = false;
-                Core::tempPlayer = Context;
-            }
-        }
+        UObjectGlobals::RegisterHook(STR("/Script/Engine.PlayerController:ClientRestart"), nullptr, posthook, nullptr);
     }
 }
